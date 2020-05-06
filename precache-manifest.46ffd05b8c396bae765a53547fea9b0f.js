8a19bf8d5f178be71fb27ca6127eed024301ffb0
Zabirov Artem <qwertysairex@gmail.com>
2020-05-06 19:50:00 +0300

    Updates

---
+++ precache-manifest.46ffd05b8c396bae765a53547fea9b0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dc2bba51dc0dd3b3b4d00332ff4a828",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "15590adbb596fda546f6",
+    "url": "/weather-app/static/css/main.fdb0cb1b.chunk.css"
+  },
+  {
+    "revision": "3caa17311e38153180bf",
+    "url": "/weather-app/static/js/2.b5540978.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/weather-app/static/js/2.b5540978.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15590adbb596fda546f6",
+    "url": "/weather-app/static/js/main.28bdf3cf.chunk.js"
+  },
+  {
+    "revision": "02b6fcb411a844846e49",
+    "url": "/weather-app/static/js/runtime-main.fc220ae5.js"
+  },
+  {
+    "revision": "246c1201edbfbe27c6efc421dbc7eec5",
+    "url": "/weather-app/static/media/background.246c1201.jpg"
+  }
+]);
